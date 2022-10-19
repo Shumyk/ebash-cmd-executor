080b09ff3fa8f126b4c0f6a9b0c0fbbfaef77ea5
Shumyk <dima.shumyk@gmail.com>
2022-10-19 10:44:04 +0300

    refactored request/response to communication.go

---
+++ communication.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	FAILED  = "failed"
+	SUCCESS = "success"
+)
+
+type ExecuteRequest struct {
+	Command string `json:"command" binding:"required"`
+}
+
+type ExecuteResponse struct {
+	Status string `json:"status"`
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
+	Error  string `json:"error"`
+}
+
+func successExecuteResponse(stdout, stderr string) *ExecuteResponse {
+	return &ExecuteResponse{SUCCESS, stdout, stderr, ""}
+}
+
+func failedExecuteBadRequest(context *gin.Context, err string) {
+	context.AbortWithStatusJSON(
+		http.StatusBadRequest,
+		&ExecuteResponse{Status: FAILED, Error: err},
+	)
+}
+func failedExecuteInternalError(context *gin.Context, stdout, stderr, err string) {
+	context.AbortWithStatusJSON(
+		http.StatusInternalServerError,
+		&ExecuteResponse{FAILED, stdout, stderr, err},
+	)
+}
--- main.go
+++ main.go
@@ -21,42 +21,10 @@ func setupRouter() *gin.Engine {
 	return router
 }
 
-type ExecuteRequest struct {
-	Command string `json:"command" binding:"required"`
-}
-
-const (
-	FAILED  = "failed"
-	SUCCESS = "success"
-)
-
-type ExecuteResponse struct {
-	Status string `json:"status"`
-	Stdout string `json:"stdout"`
-	Stderr string `json:"stderr"`
-	Error  string `json:"error"`
-}
-
-func SuccessExecuteResponse(stdout, stderr string) *ExecuteResponse {
-	return &ExecuteResponse{SUCCESS, stdout, stderr, ""}
-}
-func FailedExecuteBadRequest(context *gin.Context, err string) {
-	context.AbortWithStatusJSON(
-		http.StatusBadRequest,
-		&ExecuteResponse{Status: FAILED, Error: err},
-	)
-}
-func FailedExecuteInternalError(context *gin.Context, stdout, stderr, err string) {
-	context.AbortWithStatusJSON(
-		http.StatusInternalServerError,
-		&ExecuteResponse{FAILED, stdout, stderr, err},
-	)
-}
-
 func executePOST(context *gin.Context) {
 	request := ExecuteRequest{}
 	if err := context.BindJSON(&request); err != nil {
-		FailedExecuteBadRequest(context, fmt.Sprintf("Can't parse a body: %v", err))
+		failedExecuteBadRequest(context, fmt.Sprintf("Can't parse a body: %v", err))
 		return
 	}
 
@@ -64,10 +32,10 @@ func executePOST(context *gin.Context) {
 
 	stdout, stderr, err := execute.ExecuteCommand(request.Command)
 	if err != nil {
-		FailedExecuteInternalError(context, stdout, stderr, err.Error())
+		failedExecuteInternalError(context, stdout, stderr, err.Error())
 		return
 	}
-	context.JSON(http.StatusOK, SuccessExecuteResponse(stdout, stderr))
+	context.JSON(http.StatusOK, successExecuteResponse(stdout, stderr))
 }
 
 func PersistCommand(command string) {
