106406bbf393add67e123a497bdc7db4657657e0
Shumyk <dima.shumyk@gmail.com>
2022-10-20 11:02:18 +0300

    added graceful shotdown, cleaning up vagrants vms on shutdown

--- execute/vagrant.go
+++ execute/vagrant.go
@@ -6,12 +6,19 @@ import (
 	"github.com/bmatcuk/go-vagrant"
 )
 
+type AliveVagrants struct {
+	*vagrant.VagrantClient
+}
+
+var vagrants []AliveVagrants = make([]AliveVagrants, 0)
+
 func VagrantUp() {
 	// TODO: path to Vagrant file from properties
 	client, err := vagrant.NewVagrantClient("/Users/shumyk/codeself/shell/cmd-exe")
 	if err != nil {
 		log.Panicf("could not vagrant client ahh!! [%v]", err)
 	}
+	vagrants = append(vagrants, AliveVagrants{client})
 
 	upcmd := client.Up()
 	upcmd.Verbose = true
@@ -31,4 +38,30 @@ func VagrantUp() {
 	log.Println(status.StatusResponse.Error)
 	log.Println("vagrant status response:")
 	log.Println(status.StatusResponse.ErrorResponse)
+}
+
+func CleanUp(ch chan bool) {
+	for _, wr := range vagrants {
+		log.Printf("starting vagrant halt [%v]", wr.VagrantClient.VagrantfileDir)
+		halt := wr.VagrantClient.Halt()
+		halt.Verbose = true
+		if err := halt.Run(); err != nil {
+			log.Printf("coudn't halt vagrant [%v]", wr.VagrantClient.VagrantfileDir)
+			log.Printf("forcing vagrant halt")
+
+			forceHalt := wr.VagrantClient.Halt()
+			forceHalt.Verbose = true
+			forceHalt.Force = true
+			if err := forceHalt.Run(); err != nil {
+				log.Printf("coudn't force halt vagrant [%v]", wr.VagrantClient.VagrantfileDir)
+				log.Printf("destroying vagrant")
+
+				destroy := wr.VagrantClient.Destroy()
+				destroy.Verbose = true
+				destroy.Run()
+			}
+		}
+	}
+	log.Println("successfully halt all vagrants")
+	ch <- true
 }
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	dto "ebash/cmd-executor/communication"
 	exe "ebash/cmd-executor/execute"
 	persistant "ebash/cmd-executor/persistance"
 	"fmt"
+	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +18,37 @@ import (
 func main() {
 	go exe.VagrantUp()
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	router := setupRouter()
-	router.Run(":8080")
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go listenAndServe(server)
+	<-ctx.Done()
+
+	vagrantChannel := make(chan bool)
+	go exe.CleanUp(vagrantChannel)
+	stop()
+	log.Println("shutting down gracefully, press ctrl+c again to kill me ;(")
+
+	<-vagrantChannel
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalln("server forced to shutdown: ", err)
+	}
+
+	log.Println("adios!")
+}
+
+func listenAndServe(server *http.Server) {
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("failed to serve: [%v]", err)
+	}
 }
 
 func setupRouter() *gin.Engine {
