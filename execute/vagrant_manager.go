280402008c36f711a99257d796f31dba5f0f946a
Shumyk <dima.shumyk@gmail.com>
2022-10-21 19:48:56 +0300

    implemented async start of vms

--- execute/vagrant_manager.go
+++ execute/vagrant_manager.go
@@ -2,6 +2,7 @@ package execute
 
 import (
 	"log"
+	"strings"
 	"sync"
 
 	"github.com/bmatcuk/go-vagrant"
@@ -9,26 +10,35 @@ import (
 
 const (
 	// TODO: path to Vagrant file from properties
-	VAGRANT_PATH  = "/Users/shumyk/codeself/shell/cmd-exe"
-	VAGRANTS_SIZE = 0
+	VAGRANT_PATHS  = "/Users/shumyk/codeself/shell/cmd-exe"
+	PATH_SEPARATOR = ","
 )
 
-// TODO: 2. vagrant up in loop for several machine
-// TODO: 3. vagrant add boxes
+// TODO: 3. vagrant add boxes (?)
 // TODO: 4. test async vagrant halt with multiple instances
 
 // TODO: 5. probably move vagrant slice to pool object
-var vagrants []*AliveVagrant = make([]*AliveVagrant, VAGRANTS_SIZE)
+var vagrants []*AliveVagrant
 
 func VagrantsUp() {
-	client, err := vagrant.NewVagrantClient(VAGRANT_PATH)
-	logPanically(err, "create client")
+	paths := strings.Split(VAGRANT_PATHS, PATH_SEPARATOR)
+	for _, path := range paths {
+		go initClient(path)
+	}
+}
 
-	aliveVagrant := &AliveVagrant{client}
+func initClient(path string) {
+	aliveVagrant := &AliveVagrant{newClient(path)}
 	vagrants = append(vagrants, aliveVagrant)
 
 	aliveVagrant.Up()
-	go aliveVagrant.Status()
+	aliveVagrant.Status()
+}
+
+func newClient(path string) *vagrant.VagrantClient {
+	client, err := vagrant.NewVagrantClient(path)
+	logPanically(err, "create client")
+	return client
 }
 
 func HaltVagrants(ch chan<- bool) {
@@ -41,7 +51,7 @@ func HaltVagrants(ch chan<- bool) {
 
 	wg.Wait()
 	ch <- true
-	log.Println("successfully halt all vagrants")
+	log.Println("successfully halted all vagrants")
 }
 
 func logPanically(err error, action string) {
